87e5bded894608d1b39597baa1c2b5ae57da9a70
jlyou <youjinlong_yn@sina.com>
2020-12-18 18:25:00 +0800

    修改备注

---
+++ mybatis-plus/src/main/java/com/yjl/mybatisplus/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.yjl.mybatisplus.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yjl.mybatisplus.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * TODO
+ *
+ * @author: jlyou
+ * @date: 2020-12-18 16:22
+ **/
+@Mapper
+public interface UserMapper extends BaseMapper<User> {
+
+}
